40045ec6a65f1612a78e82cd7e21e97b4d8b3901
aashish_k <aashishkathat00@gmail.com>
2024-02-14 23:20:08 +0530

    init

---
+++ src/contexts/TodoContext.tsx
@@ -0,0 +1,86 @@
+import { FC, createContext, useContext, useEffect, useState } from 'react';
+
+interface Todo {
+	id: number;
+	todo: string;
+	completed: boolean;
+}
+interface TodoContextValue {
+	todos?: Todo[];
+	addTodo?: (todo: Todo) => void;
+	updateTodo?: (id: number, updatedTodo: Todo) => void;
+	deleteTodo?: (id: number) => void;
+	toggleComplete?: (id: number) => void;
+	setTodos?: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+export const TodoContext = createContext<TodoContextValue>({});
+
+interface MyContextProviderProps {
+	children: React.ReactNode;
+}
+export const TodoProvider: FC<MyContextProviderProps> = ({ children }) => {
+	const [todos, setTodos] = useState<Array<Todo>>([
+		{
+			id: 1,
+			todo: 'Todo msg',
+			completed: false,
+		},
+	]);
+	useEffect(() => {
+		let localData: string;
+		const getL = localStorage.getItem('todos');
+		if (getL) {
+			localData = JSON.parse(getL);
+			return;
+		}
+		if (localData && localData.length > 0) setTodos(todos);
+	}, [setTodos]);
+
+	useEffect(() => {
+		localStorage.setItem('todos', JSON.stringify(todos));
+	}, [todos]);
+
+	function addTodo(todo: Todo): void {
+		setTodos((pvev) => [...pvev, { ...todo }]);
+	}
+	function updateTodo(id: number, todo: Todo): void {
+		setTodos((prev) =>
+			prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+		);
+	}
+	function deleteTodo(id: number): void {
+		setTodos((prev) => prev.filter((todo) => todo.id !== id));
+	}
+	function toggleComplete(id: number): void {
+		setTodos((prev) =>
+			prev.map((prevTodo) =>
+				prevTodo.id === id
+					? { ...prevTodo, completed: !prevTodo.completed }
+					: prevTodo
+			)
+		);
+	}
+
+	return (
+		<TodoContext.Provider
+			value={{
+				todos,
+				setTodos,
+				addTodo,
+				updateTodo,
+				deleteTodo,
+				toggleComplete,
+			}}
+		>
+			{children}
+		</TodoContext.Provider>
+	);
+};
+
+export function useTodo() {
+	const context = useContext(TodoContext);
+	if (!context )
+		throw new Error('context used outside todoContext');
+
+	return context;
+}
